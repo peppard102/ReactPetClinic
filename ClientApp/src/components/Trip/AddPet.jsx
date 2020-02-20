aa5fd60d97fd179e62944578652be3cf9ce26f1c
Lauren Peppard <PEPPARDLL@innovasi.com>
2020-02-19 22:05:21 -0800

    Using ReactStrap for add pets page

--- ClientApp/src/components/Trip/AddPet.jsx
+++ ClientApp/src/components/Trip/AddPet.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import axios from 'axios';
+import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 
 export class AddPet extends Component {
     constructor(props) {
@@ -36,15 +37,53 @@ export class AddPet extends Component {
         return (
             <div className="trip-form">
                 <h3>Add New Pet</h3>
-                <form onSubmit={this.onSubmit}>
-                    <div className="form-group">
-                        <label>Pet name:  </label>
-                        <input type="text" className="form-control" value={this.state.name} onChange={this.onChangeName}/>
-                    </div>
-                    <div className="form-group">
-                        <input type="submit" value="Add Pet" className="btn btn-primary"/>
-                    </div>
-                </form>
+                <Form onSubmit={this.onSubmit}>
+                    <FormGroup>
+                        <Label for="petName">Pet Name</Label>
+                        <Input type="text" name="petName" id="petName" placeholder="Enter Pet Name" value={this.state.name} onChange={this.onChangeName} />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="speciesSelect">Species</Label>
+                        <Input type="select" name="speciesSelect" id="speciesSelect">
+                        <option>Cat</option>
+                        <option>Dog</option>
+                        <option>Bird</option>
+                        <option>Hamster</option>
+                        <option>Other</option>
+                        </Input>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="exampleText">Allergies</Label>
+                        <Input type="textarea" name="text" id="exampleText" />
+                    </FormGroup>
+                    <FormGroup tag="fieldset">
+                        <legend>Vaccinations Up to Date?</legend>
+                        <FormGroup check>
+                        <Label check>
+                            <Input type="radio" name="radio1" />{' '}
+                            Yes
+                        </Label>
+                        </FormGroup>
+                        <FormGroup check>
+                        <Label check>
+                            <Input type="radio" name="radio1" />{' '}
+                            No
+                        </Label>
+                        </FormGroup>
+                        <FormGroup check>
+                        <Label check>
+                            <Input type="radio" name="radio1" />{' '}
+                            Not Sure
+                        </Label>
+                        </FormGroup>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="exampleFile">Medical Records</Label>
+                        <Input type="file" name="file" id="exampleFile" />
+                    </FormGroup><br/>
+                    {/* <Button >Submit</Button> */}
+                    <input type="submit" value="Add Pet" className="btn btn-primary"/>
+                </Form>
             </div>
         )
     }
