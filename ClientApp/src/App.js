4c995627e70ecd9e33adf5719b541ccceed0288c
Lauren <peppard_lauren@yahoo.com>
2020-02-23 17:28:04 -0800

    Changed folder name

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,29 +1,29 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router';
-import { Layout } from './components/Layout';
-import { Home } from './components/Home';
+import React, { Component } from "react";
+import { Route } from "react-router";
+import { Layout } from "./components/Layout";
+import { Home } from "./components/Home";
 
-import './custom.css'
-import { AllAppointments } from './components/Trip/AllAppointments';
-import { AddPet } from './components/Trip/AddPet';
-import { AddVet } from './components/Trip/AddVet';
-import { AddAppointment } from './components/Trip/AddAppointment';
-import { Update } from './components/Trip/Update';
-import { Delete } from './components/Trip/Delete';
+import "./custom.css";
+import { AllAppointments } from "./components/PetClinic/AllAppointments";
+import { AddPet } from "./components/PetClinic/AddPet";
+import { AddVet } from "./components/PetClinic/AddVet";
+import { AddAppointment } from "./components/PetClinic/AddAppointment";
+import { Update } from "./components/PetClinic/Update";
+import { Delete } from "./components/PetClinic/Delete";
 
 export default class App extends Component {
   static displayName = App.name;
 
-  render () {
+  render() {
     return (
       <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/allAppointments' component={AllAppointments} />
-        <Route path='/addPet' component={AddPet} />
-        <Route path='/addVet' component={AddVet} />
-        <Route path='/addAppointment' component={AddAppointment} />
-        <Route path='/update/:id' component={Update} />
-        <Route path='/delete/:id' component={Delete} />
+        <Route exact path="/" component={Home} />
+        <Route path="/allAppointments" component={AllAppointments} />
+        <Route path="/addPet" component={AddPet} />
+        <Route path="/addVet" component={AddVet} />
+        <Route path="/addAppointment" component={AddAppointment} />
+        <Route path="/update/:id" component={Update} />
+        <Route path="/delete/:id" component={Delete} />
       </Layout>
     );
   }
